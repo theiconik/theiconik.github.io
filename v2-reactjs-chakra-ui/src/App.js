51ff476472e56db63a73c751978cbcbb2d655409
theiconik <kumarujjawal51@gmail.com>
2021-06-15 01:44:40 +0530

    Content almost built up with styling

--- v2-reactjs-chakra-ui/src/App.js
+++ v2-reactjs-chakra-ui/src/App.js
@@ -1,10 +1,48 @@
-import './App.css';
+import "./App.css";
+import { ChakraProvider, Heading, Text, Button, HStack } from "@chakra-ui/react";
+import {FaFacebook, FaGithub, FaLinkedinIn, FaTwitter} from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <p>Ujjawal Pandey</p>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <div className="content">
+          <Heading
+            as="h1"
+            size="4xl"
+            fontWeight="700"
+            color="#2d3748"
+            style={{ letterSpacing: "-0.1rem" }}
+          >
+            Hi, I'm
+          </Heading>
+          <Heading
+            className="name"
+            as="h1"
+            size="4xl"
+            color="#319795"
+            fontWeight="700"
+            style={{ letterSpacing: "-0.1rem" }}
+          >
+            Ujjawal Pandey
+          </Heading>
+          <Text fontSize="lg" color="gray.500" mt={12} mb={6} style={{textAlign: "center"}}>
+            Web Frontend Dev & 🤍 problem solving
+          </Text>
+          <HStack>
+            <Button colorScheme="blackAlpha" leftIcon={<FaGithub />}>
+              Github
+            </Button>
+            <Button colorScheme="twitter" leftIcon={<FaTwitter />}>
+              Twitter
+            </Button>
+            <Button colorScheme="blue" leftIcon={<FaLinkedinIn />}>
+              LinkedIn
+            </Button>
+          </HStack>
+        </div>
+      </div>
+    </ChakraProvider>
   );
 }
 
